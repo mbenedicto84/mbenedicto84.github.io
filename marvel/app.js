c526be6dc2911311cf1d19e8e588e552e3030aa3
mbenedicto84 <m.benedicto84@gmail.com>
2019-07-18 16:37:21 -0400

    api functional

--- marvel/app.js
+++ marvel/app.js
@@ -1,3 +1,22 @@
 $(() => {
-console.log($);
+
+  $('form').on('submit',(event) => {
+  const character = $('input[type="text"]').val();
+  event.preventDefault()
+  $.ajax({
+      url:'http://gateway.marvel.com/v1/public/characters?ts=thesoed&apikey=d46dd7f46b99a19182859d3ef5c9b862&hash=f34a04018516c1859aec3fcd30d5b44d&name=' + character,
+      success: (data)=>{
+          $('#name').html(data.data.results[0].name);
+            $('#bio').html(data.data.results[0].description);
+          $('#comics').html(data.data.results[0].comics.items[0].name);
+
+      },
+      error: ()=>{
+          console.log('bad request');
+      }
+  })
+
+
+
+  })
 })
