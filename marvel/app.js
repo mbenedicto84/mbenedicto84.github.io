2ef99bc243d5a5bd00bfbcb1cf9d7bd6c53b16b3
mbenedicto84 <m.benedicto84@gmail.com>
2019-07-18 17:05:57 -0400

    segment one

--- marvel/app.js
+++ marvel/app.js
@@ -7,8 +7,12 @@ $(() => {
       url:'http://gateway.marvel.com/v1/public/characters?ts=thesoed&apikey=d46dd7f46b99a19182859d3ef5c9b862&hash=f34a04018516c1859aec3fcd30d5b44d&name=' + character,
       success: (data)=>{
           $('#name').html(data.data.results[0].name);
-            $('#bio').html(data.data.results[0].description);
+          $('#bio').html(data.data.results[0].description);
           $('#comics').html(data.data.results[0].comics.items[0].name);
+          $('#series').html(data.data.results[0].series.items[4].name);
+          $('#events').html(data.data.results[0].events.items[4].name);
+
+
 
       },
       error: ()=>{
