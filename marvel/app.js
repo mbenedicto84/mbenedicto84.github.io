c69f3fbc33d46226ccde6f9d1e03eace15202bbb
mbenedicto84 <m.benedicto84@gmail.com>
2019-07-18 22:46:56 -0400

    first night push

--- marvel/app.js
+++ marvel/app.js
@@ -36,7 +36,6 @@ $closeBtn.on('click', closeModal);
       url:'http://gateway.marvel.com/v1/public/characters?ts=thesoed&apikey=d46dd7f46b99a19182859d3ef5c9b862&hash=f34a04018516c1859aec3fcd30d5b44d&name=' + character,
       success: (data)=>{
         for (var i = 0; i < 20; i++) {
-          console.log(data);
             image = (data.data.results[0].thumbnail.path)
              $div = $('<div>');
              $div.addClass('eve');
@@ -46,30 +45,26 @@ $closeBtn.on('click', closeModal);
             $('#events').append($div);
             $('#series').append($div2);
             $('#comics').append($div3);
-          $('#name').html(data.data.results[0].name);
-          $('#bio').html(data.data.results[0].description);
+            $('#name').html(data.data.results[0].name);
+            $('#bio').html(data.data.results[0].description);
 
-          $div3.append( '<div>' ).html(data.data.results[0].comics.items[i].name);
-          $div2.append( '<div>' ).html(data.data.results[0].series.items[i].name);
-          $div.append( '<div>' ).html(data.data.results[0].events.items[i].name);
+            $div3.append( '<div>' ).html(data.data.results[0].comics.items[i].name);
+            $div2.append( '<div>' ).html(data.data.results[0].series.items[i].name);
+            $div.append( '<div>' ).html(data.data.results[0].events.items[i].name);
             $('#image').html('<img src="'+image+'.jpg">');
-           console.log(image);
 
 
+            }
+        error: ()=>{
+            console.log('bad request');
 
       }
-      error: ()=>{
-          console.log('bad request');
-      }
-          }
+    }
   })
-
-
-
   })
+//------------------------------------------------------------Superpower
 
-
-
+// var superPower = ['Flight','Super Strength',"X Ray Vision","Invulnerability", "Super Intellect","Super Speed","Telekinis","Psychic Ability","Healing Ability", "Master Of Machines", "Weilder of Infinity Stones"]
 
 
 })
