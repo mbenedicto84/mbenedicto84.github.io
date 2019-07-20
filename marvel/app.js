dbc6f8f095504231446071065f114c5540fca6e5
mbenedicto84 <m.benedicto84@gmail.com>
2019-07-20 17:18:13 -0400

    added stories

--- marvel/app.js
+++ marvel/app.js
@@ -46,6 +46,7 @@ $closeBtn2.on('click', closeModal2);
   $.ajax({
       url:'https://gateway.marvel.com/v1/public/characters?ts=thesoed&apikey=d46dd7f46b99a19182859d3ef5c9b862&hash=f34a04018516c1859aec3fcd30d5b44d&name=' + character,
       success: (data)=>{
+        console.log(data);
         for (var i = 0; i < 20; i++) {
             image = (data.data.results[0].thumbnail.path)
              $div = $('<div>');
@@ -53,12 +54,14 @@ $closeBtn2.on('click', closeModal2);
              $div2 = $('<div>');
              $div2.addClass('ser');
              $div3 = $('<div>');
+              $div4 = $('<div>');
             $('#events').append($div);
             $('#series').append($div2);
             $('#comics').append($div3);
+            $('#stories').append($div4);
             $('#name').html(data.data.results[0].name);
             $('#bio').html(data.data.results[0].description);
-
+            $div4.append( '<div>' ).html(data.data.results[0].stories.items[i].name);
             $div3.append( '<div>' ).html(data.data.results[0].comics.items[i].name);
             $div2.append( '<div>' ).html(data.data.results[0].series.items[i].name);
             $div.append( '<div>' ).html(data.data.results[0].events.items[i].name);
